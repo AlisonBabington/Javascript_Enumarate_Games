e34ed9a3eceefa09e5993ab449986a7d5bd37665
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-05 16:19:39 +0100

    isogram finder comapleted

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+   return this.word.every ((letter, index) => {
+      return this.word.indexOf(letter) === index
+   });
 }
 
 module.exports = IsogramFinder;
