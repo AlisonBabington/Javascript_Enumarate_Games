edda3943fa8a81046d4dd995338e9c31ab76e550
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-05 17:03:57 +0100

    anagram finder done

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,21 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.toLowerCase();
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  let result = otherWords.filter ((wordToSearch) => {
+    return wordToSearch.length === this.word.length
+  });
+  result = result.filter ((wordToSearch) => {
+    return wordToSearch.toLowerCase() !== this.word
+  });
+  result = result.filter ((wordToSearch) => {
+    return wordToSearch.toLowerCase().split('').every ((letter) => {
+       return this.word.includes(letter)
+    })
+  });
+return result
 }
+
 
 module.exports = AnagramFinder;
